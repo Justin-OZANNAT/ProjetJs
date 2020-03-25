529abf56e74ff4db0395574644043d634f817106
Ozannat <jozannat13@gmail.com>
2020-03-25 16:11:01 +0100

    ajout date, cookies

--- php/login.php
+++ php/login.php
@@ -1,22 +1,27 @@
 <?php
 
-session_start();
+session_start(); // initialisation de la session
 $message = 'Connecté';
 
 $obj = new stdClass();
 $obj->success = false;
-$username = $_POST['username'];
-$password = $_POST['password'];
+$username = $_POST['username']; // la variable récupère la valeur Username du form
+$password = $_POST['password']; // idem pour le MDP
+echo date('d/m/y h:i:s') . "\n"; // affichage de la date
+echo 'Ton username est : ' .htmlspecialchars($username) . "\n" . 'Ton password est : ' . $password . "\n"; // affichage username et mdp
 
-echo 'Ton username est : ' .htmlspecialchars($username) . "\n" . 'Ton password est : ' . $password . "\n";
 
-if ($username == 'admin' && $password == 'admin'){
+if ($username == 'admin' && $password == 'admin'){ // on vérifie les identifiants
     $obj->success = true;
-    $_SESSION['user'] = 123;
+    $_SESSION['admin'] = 123;
     echo 'Bonne combinaison MDP / USNM';
 } else {
-    echo 'Mauvaise combinaison MDP/USNM';
-}
+    echo 'Mauvaise combinaison MDP/USNM' . "\n";
+} // condition pour afficher la réussite de la combinaison ou non
+
+setcookie('username', $username, time() + 365*24*3600, null ,null ,false ,true);
+setcookie('password', $password, time() + 365*24*3600, null ,null ,false ,true);
+
 
 header('Cache-Control: no-cache, must-revalidate');
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
